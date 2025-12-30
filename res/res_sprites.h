b3316f092aff5f7e97f1de9638395fc6d2850e61
Vincent Mariucci <mariucci1@gmail.com>
2025-12-30 18:06:58 -0500

    rendering player sprite on screen

---
+++ res/res_sprites.h
@@ -0,0 +1,8 @@
+#include <genesis.h>
+
+#ifndef _RES_RES_SPRITES_H_
+#define _RES_RES_SPRITES_H_
+
+extern const SpriteDefinition player_sprite;
+
+#endif // _RES_RES_SPRITES_H_
--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <genesis.h>
 #include <res_maps.h>
+#include <res_sprites.h>
 #include <string.h>
 
 // maps (BGA and BGB)
 Map *bgb;
 Map *bga;
+
+// sprites
+Sprite *player;
 
 // BG start tile index
 u16 bgBaseTileIndex[2];
@@ -22,17 +26,24 @@ int main(bool hardReset)
     ind += bgb_tileset.numTile;
 
     PAL_setPalette(PAL0, palette_bga.data, DMA);
-    PAL_setPalette(PAL1, palette_bgb.data, DMA);
+    PAL_setPalette(PAL3, player_sprite.palette->data, DMA);
     bga = MAP_create(&bga_map, BG_A, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, bgBaseTileIndex[0]));
-    bgb = MAP_create(&bgb_map, BG_B, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, bgBaseTileIndex[1]));
-    MAP_scrollTo(bga, 0, 0);
-    SYS_doVBlankProcess();
-    MAP_scrollTo(bgb, 0, 0);
-    SYS_doVBlankProcess();
+    bgb = MAP_create(&bgb_map, BG_B, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, bgBaseTileIndex[1]));
+    MAP_scrollTo(bga, 0, 0); //first call loads the bga tiles into VRAM
+    SYS_doVBlankProcess(); //call to clear the DMA cache
+    MAP_scrollTo(bgb, 0, 0); //first call loads the bgb tiles into VRAM
+    SYS_doVBlankProcess(); //call to clear the DMA cache again
+
+    SYS_showFrameLoad(TRUE);
+
+    // Sprites
+    SPR_init();
+    player = SPR_addSprite(&player_sprite, 40, 100, TILE_ATTR(PAL3, 0, FALSE, FALSE));
 
     while (1)
     {
 
+        SPR_update();
         SYS_doVBlankProcess();
     }
     return (0);
