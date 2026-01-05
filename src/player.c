ccbaec3c6a30ab82fd023dc5e32767a27b4ee258
Vincent Mariucci <mariucci1@gmail.com>
2026-01-04 19:42:52 -0500

    working player falling and mid-jump movement

--- src/player.c
+++ src/player.c
@@ -11,20 +11,24 @@ const int RIGHT_EDGE = 320;
 const int TOP_EDGE = 0;
 const int BOTTOM_EDGE = 100;
 
-const int WALKING_SPEED = 2; //pixels/frame
-
 const int STANDING_ANIM = 0;
 const int WALKING_ANIM = 1;
 const int DODGING_ANIM = 2;
 const int JUMPING_ANIM = 3;
 
+const int WALKING_SPEED = 2; // pixels/frame
+const int FALLING_X_SPEED = 1;
 const fix16 GRAVITY = FIX16(0.24);
 const fix16 JUMP_SPEED = FIX16(3.5);
+const u8 DEFAULT_DODGE_COOLDOWN = 54;
+const u8 MAX_DODGE_FRAMES = 30;
 
 // player sprite
 Sprite *player;
 
 // states
+s8 xOrder = 0;
+s8 yOrder = 0;
 int player_vel_x = 0;
 int player_pos_x = 40;
 fix16 player_vel_y = FIX16(0);
@@ -33,10 +37,8 @@ const int PLAYER_WIDTH = 24;
 const int PLAYER_HEIGHT = 32;
 s8 player_direction = RIGHT;
 u8 dodgeFrames = 0;
-u8 dodgeCooldown = 60;
+u8 dodgeCooldown = 0;
 u8 jumpsLeft = 2;
-char str_vel_y[3];
-// char str_playerDirection[2] = "0";
 
 static void jump(void);
 static void stand(void);
@@ -44,6 +46,8 @@ static void run(s8 direction);
 static void positionPlayer(void);
 static void handleDodge(void);
 static bool airborne(void);
+static void debug(int value);
+static void pollDpad(void);
 
 /*
 gets called upon game reset
@@ -61,23 +65,71 @@ gets called every frame by the game driver
 */
 void PLAYER_update(void)
 {
+    pollDpad();
     handleDodge();
     positionPlayer();
+    debug((int) dodgeFrames);
+}
+
+void debug(int value)
+{
+    char str_value[10];
+    sprintf(str_value, "%d", value);
+    VDP_clearText(1, 2, 10);
+    VDP_drawText(str_value, 1, 2);
+}
+
+void pollDpad()
+{
+    u16 value = JOY_readJoypad(JOY_1);
+
+    if (value & BUTTON_UP)
+        yOrder = -1;
+    else if (value & BUTTON_DOWN)
+        yOrder = +1;
+    else
+        yOrder = 0;
+
+    if (value & BUTTON_LEFT)
+        xOrder = -1;
+    else if (value & BUTTON_RIGHT)
+        xOrder = +1;
+    else
+        xOrder = 0;
 }
 
 void positionPlayer()
 {
+    // calculate velocity
+    if (xOrder == 0 && dodgeFrames == 0 && !airborne())
+        stand();
+    else if (airborne() && ((xOrder == RIGHT && player_direction == LEFT) || ((xOrder == LEFT && player_direction == RIGHT))))
+        player_vel_x = xOrder * FALLING_X_SPEED; //go slower if changing direction while falling
+    else if (dodgeFrames == 0) //on the ground, not rolling, with dpad input
+    {
+        player_vel_x = xOrder * WALKING_SPEED;
+        run(xOrder);
+    }
+
     // Add the player's velocity to its position
     player_pos_x += player_vel_x;
     player_pos_y += player_vel_y;
 
-    //apply gravity
+    // apply gravity
     if (airborne())
-    {
         player_vel_y += GRAVITY;
-    } else 
-    {
+    else
         player_vel_y = 0;
+
+    // handle falling
+    if (dodgeFrames == 0 && player_vel_y > FIX16(0))
+    {
+        SPR_setAutoAnimation(player, FALSE);
+        SPR_setAnimAndFrame(player, JUMPING_ANIM, 2); // last animation of the jump
+    }
+    else
+    {
+        SPR_setAutoAnimation(player, TRUE);
     }
 
     // Keep the player within the bounds of the screen
@@ -85,8 +137,8 @@ void positionPlayer()
         player_pos_x = LEFT_EDGE;
     if (player_pos_x + PLAYER_WIDTH > RIGHT_EDGE)
         player_pos_x = RIGHT_EDGE - PLAYER_WIDTH;
-    if (player_pos_y > F16(BOTTOM_EDGE)) player_pos_y = F16(BOTTOM_EDGE);
-
+    if (player_pos_y > F16(BOTTOM_EDGE))
+        player_pos_y = F16(BOTTOM_EDGE);
 
     // Let the Sprite engine position the sprite
     SPR_setPosition(player, player_pos_x, F16_toRoundedInt(player_pos_y));
@@ -94,33 +146,13 @@ void positionPlayer()
 
 void handleDodge()
 {
+
     if (dodgeFrames > 0) dodgeFrames--;
     if (dodgeCooldown > 0) dodgeCooldown--;
 
-    if (dodgeFrames == 1)
-    {
-        // poll controller for held input
-        u16 value = JOY_readJoypad(JOY_1);
-        if ((value & BUTTON_LEFT) && player_direction == LEFT)
-        {
-            //if LEFT or RIGHT is held, continue run
-            run(LEFT);
-        } else if ((value & BUTTON_RIGHT) && player_direction == RIGHT)
-        {
-            run(RIGHT);
-        } else
-        {
-            // no held d-pad, make player stop and stand
-            stand();
-        }
-    }
-    // debug stuff
-    sprintf(str_vel_y,"%d",F16_toRoundedInt(player_vel_y));
-    // sprintf(str_playerDirection,"%d",player_direction);
-    VDP_clearText(1,2,10);
-    // VDP_clearText(1,3,2);
-    VDP_drawText(str_vel_y,1,2);
-    // VDP_drawText(str_playerDirection,1,3);
+    // dodge interrupts
+    if ((xOrder == RIGHT && player_direction == LEFT) || (xOrder == LEFT && player_direction == RIGHT))
+        dodgeFrames = 0;
 }
 
 /*
@@ -129,7 +161,7 @@ changed = 1 if state is different than last frame's state, 0 if not
 */
 void PLAYER_doJoyAction(u16 changed, u16 pressed)
 {
-    //NOTE: need to use bitwise operators here, since enums are hex
+    // NOTE: need to use bitwise operators here, since enums are hex
 
     // handle uninterruptible motions
     if (FALSE)
@@ -139,35 +171,17 @@ void PLAYER_doJoyAction(u16 changed, u16 pressed)
 
     if (changed & pressed & BUTTON_C)
     {
-        if (dodgeCooldown > 0) return; //cannot roll until cooldown timer expires
+        if (dodgeCooldown > 0)
+            return;                                      // cannot roll until cooldown timer expires
         player_vel_x = player_direction * WALKING_SPEED; // roll at roll speed, even if starting from rest
         SPR_setAnim(player, DODGING_ANIM);
-        dodgeFrames = 30;
-        dodgeCooldown = 60;
+        dodgeFrames = MAX_DODGE_FRAMES;
+        dodgeCooldown = DEFAULT_DODGE_COOLDOWN;
     }
     else if (changed & pressed & BUTTON_B)
     {
         jump();
-    }
-    else if (pressed & BUTTON_RIGHT)
-    {
-        if (player_direction == RIGHT && dodgeFrames > 0) return; //if already dodging in this direction
-        run(RIGHT);
-        dodgeFrames = 0; //cancel dodge frames due to change in direction
-    }
-    else if (pressed & BUTTON_LEFT)
-    {
-        if (player_direction == LEFT && dodgeFrames > 0) return; //if already dodging in this direction
-        run(LEFT);
-        dodgeFrames = 0; //cancel dodge frames due to change in direction
-    }
-    else
-    {
-        if ((changed & BUTTON_RIGHT) | (changed & BUTTON_LEFT))
-        {
-            stand();
-            dodgeFrames = 0;
-        }
+        dodgeFrames = 0; // dodge cancel
     }
 }
 
@@ -187,23 +201,26 @@ void stand()
 
 void jump()
 {
-    if(!airborne())
+    if (!airborne())
     {
         jumpsLeft = 2;
         SPR_setAnim(player, JUMPING_ANIM);
         jumpsLeft--;
         player_vel_y = -JUMP_SPEED;
-    } else if (airborne() && jumpsLeft > 0)
+    }
+    else if (airborne() && jumpsLeft > 0)
     {
         SPR_setAnimAndFrame(player, JUMPING_ANIM, 0);
         jumpsLeft--;
         player_vel_y = -JUMP_SPEED;
-    } else {
-        //do nothing
+    }
+    else
+    {
+        // do nothing
     }
 }
 
-bool airborne() 
+bool airborne()
 {
     return F16_toRoundedInt(player_pos_y) < 100;
 }
